21108b264907139513646d6bcd32ba3b5d91952d
David Landeo <david.landeo@pucp.edu.pe>
2023-07-26 14:50:05 +0200

    as

---
+++ Chef.py
@@ -0,0 +1,40 @@
+import streamlit as st
+from PIL import Image
+
+st.set_page_config(
+    page_title="HealthyChef App",
+    page_icon="👨‍🍳",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        'Get Help': 'https://www.linkedin.com/in/david-landeo/',
+        'Report a bug': "https://www.linkedin.com/in/david-landeo/",
+        'About': "Esta app es para la postulación a un puesto de trabajo en PepsiCo.\
+            El contenido de esta app no le pertenece a dicha empresa."
+    }
+)
+
+image1 = Image.open('Pics/logo_pepsico_positive.jpg')
+image2 = Image.open('Pics/logo.png')
+col1, col2 = st.columns(2)
+col1.image(image1)
+col2.image(image2)
+
+st.title('Healthy Chef 👨‍🍳')
+
+st.subheader('PepsiCo Positive es una estrategia de sostenibilidad y responsabilidad corporativa de la empresa\
+            para impulsar un impacto positivo en la sociedad y el medio ambiente.🌍')
+
+st.subheader('Uno de nuestros pilares es :blue[Positive Choices]')
+
+st.subheader('Parte del propósito de Positive Choices implica aprovechar nuestras marcas para _educar_ a los consumidores\
+            e impulsar acciones positivas.✨')
+
+st.subheader('Sabemos que nuestros productos te acompañan siempre, pero también queremos que te alimentes saludablemente. 💙')
+
+st.subheader('¿Estás listo para comer saludable? 🚀')
+
+if st.button('¡Estoy listo!'):
+    image3 = Image.open('Pics/pic_bar.png')
+    st.markdown('👈 Haz click en la sección Chef en el menú lateral izquierdo')
+    st.image(image3)
