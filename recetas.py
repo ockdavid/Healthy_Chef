1a784bf793a493610fefe4646435b910639f42dd
David Landeo <david.landeo@pucp.edu.pe>
2023-08-20 03:44:22 +0200

    recetas

--- recetas.py
+++ recetas.py
@@ -1,13 +1,21 @@
+# Ensalada de Quinua y Aguacate con Salmón a la Parrilla
 receta_1 ="""Ingredientes:
 
-1 taza de quinua cocida
-1 filete de salmón a la parrilla
-1 aguacate maduro, cortado en cubos
-1 taza de espinaca fresca
-1/4 de taza de almendras picadas
-Jugo de limón
-Aceite de oliva virgen extra
-Sal y pimienta al gusto
+• 1 taza de quinua cocida
+
+• 1 filete de salmón a la parrilla
+
+• 1 aguacate maduro, cortado en cubos
+
+• 1 taza de espinaca fresca
+
+• 1/4 de taza de almendras picadas
+
+• Jugo de limón
+
+• Aceite de oliva virgen extra
+
+• Sal y pimienta al gusto
 
 Instrucciones:
 
@@ -16,3 +24,225 @@ Instrucciones:
 3. Coloca el filete de salmón a la parrilla encima de la ensalada.
 4. Rocía con jugo de limón, aceite de oliva, sal y pimienta al gusto.
 5. Sirve inmediatamente."""
+
+# Batido de Avena, Plátano y Fresas
+receta_2 = """
+Ingredientes:
+
+• 1 plátano maduro
+
+• 1/2 taza de fresas frescas
+
+• 1/4 de taza de avena
+
+• 1 taza de yogur griego sin grasa
+
+• Miel o jarabe de arce al gusto (opcional)
+
+• Cubitos de hielo (opcional)
+
+Instrucciones:
+
+1. En una licuadora, combina el plátano, las fresas, la avena y el yogur griego.
+2. Agrega miel o jarabe de arce si deseas un toque de dulzura adicional.
+3. Si prefieres una textura más fría, añade cubitos de hielo.
+4. Licúa hasta que el batido esté suave y cremoso.
+5. Sirve en un vaso y disfruta como desayuno o merienda.
+"""
+
+# Lentejas con Espinacas y Tomates
+receta_3 = """
+Ingredientes:
+
+• 1 taza de lentejas secas
+
+• 2 tazas de espinacas frescas
+
+• 1 lata de tomates triturados
+
+• 1 cucharada de aceite de oliva virgen extra
+
+• 1 cucharadita de ajo picado
+
+• 1 cucharadita de comino molido
+
+• Sal y pimienta al gusto
+
+Instrucciones:
+
+1. Cocina las lentejas según las instrucciones del paquete.
+2. En una sartén grande, calienta el aceite de oliva y saltea el ajo y el comino hasta que estén fragantes.
+3. Agrega las espinacas y saltea hasta que se marchiten.
+4. Incorpora los tomates triturados y cocina durante unos minutos.
+5. Mezcla las lentejas cocidas con la mezcla de espinacas y tomates.
+6. Cocina a fuego lento durante unos minutos para que los sabores se mezclen.
+7. Sirve caliente como plato principal o acompañamiento.
+"""
+
+# Salmón al Horno con Costra de Almendras
+
+receta_4 = """
+Ingredientes:
+
+• Filetes de salmón
+
+• Almendras trituradas
+
+• Jugo de limón
+
+• Ajo picado
+
+• Perejil fresco picado
+
+• Aceite de oliva virgen extra
+
+Instrucciones:
+
+1. Precalienta el horno a 180°C.
+2. Coloca los filetes de salmón en una bandeja para hornear.
+3. Mezcla las almendras trituradas, el jugo de limón, el ajo picado, el perejil y el aceite de oliva en un tazón hasta formar una pasta.
+4. Extiende la mezcla de almendras sobre los filetes de salmón.
+5. Hornea en el horno precalentado durante 15-20 minutos o hasta que el salmón esté cocido y la costra de almendras esté dorada.
+6. Sirve caliente y disfruta.
+"""
+
+# Tazón de Acai con Frutas y Granola
+
+receta_5 = """
+Ingredientes:
+
+• Puré de acai (congelado o en polvo)
+• Plátano
+• Fresas o bayas mixtas
+• Granola casera
+• Miel (opcional)
+
+Instrucciones:
+
+1. En un tazón, coloca el puré de acai.
+2. Decora con rodajas de plátano, fresas o bayas, y granola al gusto.
+3. Rocía miel por encima para endulzar si lo deseas.
+4. Disfruta de este saludable tazón de acai como desayuno o merienda."""
+
+# Pollo a la Parrilla con Salsa de Aguacate y Limón
+
+receta_6 = """
+Ingredientes:
+
+• Pechugas de pollo a la parrilla
+
+• Aguacate maduro
+
+• Jugo de limón
+
+• Ajo picado
+
+• Perejil fresco picado
+
+• Sal y pimienta al gusto
+
+Instrucciones:
+
+1. Prepara las pechugas de pollo a la parrilla según tu preferencia.
+2. Mientras tanto, en una licuadora, combina el aguacate, el jugo de limón, el ajo picado, el perejil, la sal y la pimienta. Licúa hasta obtener una salsa suave.
+3. Sirve las pechugas de pollo con la salsa de aguacate y limón por encima.
+4. Disfruta de este plato sabroso y saludable."""
+
+# Ensalada de Espinacas y Fresas con Vinagreta de Yogur
+
+receta_7 = """
+Ingredientes:
+
+• Espinacas frescas
+
+• Fresas frescas
+
+• Yogur griego sin grasa para la vinagreta
+
+• Almendras picadas
+
+• Miel para endulzar la vinagreta
+
+• Semillas de chia (opcional)
+
+Instrucciones:
+
+1. Lava y corta las espinacas y las fresas.
+2. Prepara una vinagreta mezclando el yogur griego con miel.
+3. Vierte la vinagreta sobre las espinacas y las fresas.
+4. Espolvorea almendras picadas y semillas de chia si lo deseas.
+5. Sirve y disfruta de esta refrescante ensalada."""
+
+# Batido de Espinacas, Plátano y Mantequilla de Almendras
+
+receta_8 = """
+Ingredientes:
+
+• Espinacas frescas
+
+• Plátano maduro
+
+• Mantequilla de almendras
+
+• Leche de almendra (o cualquier leche vegetal)
+
+• Miel (opcional)
+
+Instrucciones:
+
+1. En una licuadora, combina las espinacas, el plátano, la mantequilla de almendras y la leche de almendra.
+2. Agrega miel al gusto para endulzar.
+3. Licúa hasta obtener una textura suave y cremosa.
+4. Sirve y disfruta como desayuno o merienda saludable.
+"""
+
+# Quinua con Verduras al Curry
+
+receta_9 = """
+Ingredientes:
+
+• Quinua cocida
+
+• Brócoli
+
+• Zanahorias
+
+• Pimiento rojo
+
+• Leche de coco
+
+• Curry en polvo
+
+• Sal y pimienta al gusto
+
+Instrucciones:
+
+1. Cocina la quinua según las instrucciones del paquete.
+2. En una sartén grande, saltea el brócoli, las zanahorias y el pimiento rojo con curry en polvo.
+3. Agrega la quinua cocida y la leche de coco.
+4. Cocina a fuego lento hasta que todos los ingredientes estén bien mezclados y calientes.
+5. Añade sal y pimienta al gusto.
+6. Sirve como un plato saludable y sabroso.
+"""
+
+# Postre de Yogur y Fresas con Miel y Nueces
+
+receta_10 = """
+Ingredientes:
+
+• Yogur griego sin grasa
+
+• Fresas frescas o congeladas
+
+• Miel
+
+• Nueces picadas
+
+Instrucciones:
+
+1. En un tazón, coloca el yogur griego.
+2. Agrega las fresas y rocía con miel.
+3. Espolvorea nueces picadas por encima.
+4. Disfruta de este postre saludable y refrescante.
+"""
+
